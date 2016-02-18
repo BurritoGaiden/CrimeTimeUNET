6705bd7eb7d9a151fd0e6494edae11ff4783a6fc
FomTarro <twfarro@wpi.edu>
2016-02-17 22:06:04 -0500

    Scene fixups
    Just dumb moron polish

--- Assets/UI Scripts/FilmFlicker.cs
+++ Assets/UI Scripts/FilmFlicker.cs
@@ -12,6 +12,16 @@ public class FilmFlicker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        cr.alpha = Random.Range(0.0f, 0.11f);
-	}
+        cr.alpha = Random.Range(0.5f, 1.0f);
+    }
+
+    IEnumerator Flicker()
+    {
+        do
+        {
+            cr.alpha = Random.Range(0.5f, 1.0f);
+            yield return new WaitForSeconds((1.0f / 30.0f));
+        } while (true);
+    }
+
 }
